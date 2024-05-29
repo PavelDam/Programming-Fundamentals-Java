83ae7c2715393fea0bef9e52b3129de00572d90f
Pavel Damyanov <pavel_dam@outlook.com>
2024-05-29 14:38:34 +0300

    Adding the solved tasks from the lab: DataTypesAndVariables

---
+++ _02_DataTypesAndVariables/Lab/p12_RefactorSpecialNumbers.java
@@ -0,0 +1,30 @@
+package L02DataTypesAndVariables.Lab;
+
+import java.util.Scanner;
+
+public class P12RefactorSpecialNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= num; i++) {
+            int digitSum = 0;
+
+            int currentNum = i;
+            while (currentNum > 0) {
+                digitSum += currentNum % 10;
+                currentNum = currentNum / 10;
+            }
+            boolean isSpaecial = (digitSum == 5) || (digitSum == 7) || (digitSum == 11);
+
+            if (isSpaecial) {
+                System.out.printf("%d -> True%n", i);
+
+            } else {
+                System.out.printf("%d -> False%n", i);
+            }
+        }
+
+    }
+}
